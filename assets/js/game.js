216e3a85d66f32bf4cfbd0cf1145a26c3efab399
mackankowski <mackankowski@gmail.com>
2017-12-14 00:07:56 +0100

    Add Show hint button with logic

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,7 +2,8 @@ class Game {
 
     constructor(ref, response) {
         
-        this.tries = data.tries;        
+        this.word;
+        this.tries = data.tries;      
         this.randomizeWord();
         this.render(ref, response);
         
@@ -14,13 +15,16 @@ class Game {
         document.querySelector(ref.content).innerHTML += '<p>Category: ' + this.word.category + '</p>'; 
         document.querySelector(ref.content).innerHTML += '<p class="tries">Tries: ' + this.tries + '</p>'; 
         
-
     }
 
     randomizeWord() {
 
         this.word = data.words[Math.floor(Math.random() * data.words.length)];
 
+    }
+
+    showHint() {
+        document.querySelector(".hint").innerHTML = "<p>Hint: " + this.word.hint + "</p>";
     }
 
 }
