5c61f6b70d7dfd73ecb8e65d66bcf3b1679c79fd
mackankowski <mackankowski@gmail.com>
2017-12-13 03:45:18 +0100

    HM-5: Add the best times screen

--- js/view.js
+++ js/view.js
@@ -11,10 +11,11 @@ class View {
         this.ajaxRequest(this);
         this.backButtonDisplay();
 
+
     }
 
     ajaxRequest(ref) {
-
+        
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
@@ -23,15 +24,20 @@ class View {
                     case 'main':
                         document.getElementById(ref.dest).innerHTML = this.responseText;
                         break;
+                    case 'game':
+                        document.getElementById(ref.dest).innerHTML = this.responseText;
+                        break;
                     case 'categories':
                         ref.loadCategories(ref, this.responseText);
                         break;
                     case 'scoreboard':
-                        //ref.loadScoreboard(ref, this.responseText);
+                        ref.loadScoreboard(ref, this.responseText);
                         break;
                     default:
-                        document.getElementById(ref.dest).innerHTML = "Content not found!";
+                        document.getElementById(ref.dest).innerHTML = "<h2>Content not found!</h2>";
                 }
+
+                
 
             }
         };
@@ -53,10 +59,53 @@ class View {
         categories = categories.filter((v, i, a) => a.indexOf(v) === i); 
 
         for (let i in categories) {
-            document.getElementById(ref.dest).innerHTML += "<button class='categories'>" + categories[i] + "</button>";
+            document.getElementById(ref.dest).innerHTML += "<button class='category'>" + categories[i] + "</button>";
         }
     }
 
+    loadScoreboard(ref, response) {
+
+        //this.saveScoreboard(); // test data
+
+        document.getElementById(ref.dest).innerHTML = response;
+
+        var obj = JSON.parse(localStorage.getItem('scoreboard'));
+
+        obj.sort((a, b) => parseFloat(a.score) - parseFloat(b.score));
+
+        if (obj !== "undefined") {
+            for (let i in obj) {
+                
+                document.getElementById(ref.dest).innerHTML += "<button class='score'>" + obj[i].name + ' (' + obj[i].score + ')</button>';
+    
+            }
+        } else {
+
+            document.getElementById(ref.dest).innerHTML += "<p>No scores</p>";
+
+        }
+
+    }
+
+    saveScoreboard() {
+
+        // test data
+
+        // add first user
+
+        var arr = [];
+        var u1 = {name: 'Maciej', score: 19.40};
+        arr.push(u1);
+        localStorage.setItem('scoreboard', JSON.stringify(arr));
+
+        // add second user
+     
+        var u2 = {name: "Jan", score: 15.20};
+        arr = JSON.parse(localStorage.getItem('scoreboard'));
+        arr.push(u2);
+        localStorage.setItem('scoreboard', JSON.stringify(arr));
+
+    }
 
     backButtonDisplay() {
 
@@ -69,7 +118,7 @@ class View {
     }
 
     refreshView(id) {
-        
+
         this.id = id;
         this.loadTemplate();
 
