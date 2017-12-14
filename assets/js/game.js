eca5f5bb83cda39c2d7e927d72d1133d9ea75001
mackankowski <mackankowski@gmail.com>
2017-12-14 01:07:06 +0100

    Add Guess area inputs

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,14 +6,16 @@ class Game {
         this.tries = data.tries;      
         this.randomizeWord();
         this.render(ref, response);
+        this.drawGuessArea();
         
     }
 
     render(ref, response) {
 
         document.querySelector(ref.section).innerHTML = response;
-        document.querySelector(ref.content).innerHTML += '<p>Category: ' + this.word.category + '</p>'; 
-        document.querySelector(ref.content).innerHTML += '<p class="tries">Tries: ' + this.tries + '</p>'; 
+        document.querySelector(".category").innerHTML += 'Category: ' + this.word.category; 
+        document.querySelector(".tries").innerHTML += 'Tries: ' + this.tries ; 
+        
         
     }
 
@@ -23,9 +25,32 @@ class Game {
 
     }
 
-    showHint() {
-        document.querySelector(".hint").innerHTML = "<p>Hint: " + this.word.hint + "</p>";
+    drawGuessArea() {
+
+        for (var i = 0; i < this.word.title.length; i++) {
+
+            if (this.word.title[i] == ' ') {
+
+                document.querySelector(".guessArea").innerHTML += "<br/>";
+
+            } else {
+
+                document.querySelector(".guessArea").innerHTML += "<input type='text' name='" + i + "'>";
+
+            }
+
+        }
+
     }
+
+    showHint() {
+        document.querySelector(".hint").style.display = 'inline-block';
+        document.querySelector(".hint").innerHTML = "Hint: " + this.word.hint;
+        document.querySelector(".hintBtn").style.display = 'none';
+    }
+    
+
+    
 
 }
 
